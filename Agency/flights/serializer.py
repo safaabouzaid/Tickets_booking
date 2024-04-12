b3082506e67b4f27d66c96ca860e808dba3c1b74
safaabouzaid <abouzaidsafa@gmail.com>
2024-04-13 00:32:43 +0300

    >.

--- Agency/flights/serializer.py
+++ Agency/flights/serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Flight,Review,SeatType
-
+'''
 class FlightSerializer(serializers.ModelSerializer) :
     
     reviws=serializers.SerializerMethodField(method_name='get_reviews',read_only=True)
@@ -12,7 +12,7 @@ class FlightSerializer(serializers.ModelSerializer) :
         reviews=obj.reviews.all()
         serializer=ReviewSerializer(reviews,many=True)
         return serializer.data
-    
+'''    
 class ReviewSerializer(serializers.ModelSerializer) : 
     class Meta:
        
@@ -23,7 +23,7 @@ class FlightSerializer(serializers.ModelSerializer) :
     
     class Meta:
         model=Flight
-        fields=['return_date','duration','notes','ratings']        
+        fields='__all__'       
 
 
 
