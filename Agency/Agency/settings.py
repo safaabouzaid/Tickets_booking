3e24a1ca19d34e14ff9b0bfc5dee10e93d9cc792
Sedraalsabagh <sedraalsabbagh2021@gmail.com>
2024-04-17 23:59:05 +0300

    s

--- Agency/Agency/settings.py
+++ Agency/Agency/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-9^b0d(#@7kgnanuyhbfp0+y1kz3cnx!f72a9wa%x-^-j$_02x2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
